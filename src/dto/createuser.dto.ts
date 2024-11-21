8dc01afae5f31520754a9314b7b556e874a78fb5
Robert Amoah <mr_robertamoah@yahoo.com>
2024-11-21 23:53:42 +0000

    added User model to prisma schema and upated UsersService to have findUserByEmail and createUser methods

---
+++ src/dto/createuser.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDTO {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  id: number;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from 'src/dto/createuser.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 export type User = any;
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      id: 1,
-      firstName: 'Robert',
-      lastName: 'Amoah',
-      otherNames: '',
-      email: '',
-      password: '',
-    },
-  ];
+  constructor(private prisma: PrismaService) {}
 
-  async findOne(email: string): Promise<User | undefined> {
-    return this.users.find((user) => user.email == email);
+  async findUserByEmail(email: string): Promise<User | undefined> {
+    return await this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDTO.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: { email: createUserDTO.email, password: hashedPassword },
+    });
+
+    delete user.password;
+
+    return user;
   }
 }
