a17380e8f1ae2864d0c12f02bc387fd4eacb865b
Robert Amoah <mr_robertamoah@yahoo.com>
2024-12-02 20:13:11 +0000

    made corrections to the imports and exports fields of Module function in the emails.module

--- src/emails/emails.module.ts
+++ src/emails/emails.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EmailsController } from './emails.controller';
 import { EmailsService } from './emails.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [EmailsController],
   providers: [EmailsService],
-  imports: [EmailsService],
+  imports: [PrismaModule],
+  exports: [EmailsService],
 })
 export class EmailsModule {}
